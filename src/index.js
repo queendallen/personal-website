9ef71ebc601e65ec168c5e279db4b7590bb43f07
Dyandra Allen <dallen9@binghamton.edu>
2021-10-03 15:49:50 -0400

    Removed sass and added build

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const DATA = [
-  {id: nanoid(), name: "SA Travel Form", projType: "work", skills:["Vanilla JS", "HTML", "CSS", "MySQL", "PHP"], desc:"Travel form for Binghamton University's Student Association", githubLink:"", link:"https://www.forms.binghamtonsa.org/travel/"},
+  {id: nanoid(), name: "SA Travel Form", projType: "work", skills:["Vanilla JS", "HTML", "CSS", "MySQL", "PHP"], desc:"Travel form for Binghamton University's Student Association. Note: No Github Link", githubLink:"", link:"https://www.forms.binghamtonsa.org/travel/"},
   {id: nanoid(), name: "SA Timesheet System", projType: "work", skills:["Ruby on Rails", "JS"], desc:"Timesheet for Student Association employees", githubLink:"https://github.com/MatthewDamiata/SA-Timesheet-System", link:"https://time.binghamtonsa.org"},
   {id: nanoid(), name: "Portfolio", projType: "personal", skills:["React","HTML", "CSS"], desc:"My website/portfolio", githubLink:"https://github.com/queendallen/personal-website", link:""},
-  {id: nanoid(), name: "Knits N Things", projType: "personal", skills:["React","HTML", "CSS"], desc:"WIP. Mock Ecommerce website dedicated to handmade crochet projects", githubLink:"https://github.com/queendallen/knitsnthings", link:""},
+  {id: nanoid(), name: "Knits N Things", projType: "personal", skills:["React","HTML", "CSS"], desc:"WIP. Mock Ecommerce website dedicated to handmade crochet projects. Note: Not Github Link and No Live", githubLink:"https://github.com/queendallen/knitsnthings", link:""},
   {id: nanoid(), name: "Weather App", projType: "personal", skills:["React", "HTML", "CSS"], desc:"WIP. Gives 3-day forecast for any inputted location", githubLink:"https://github.com/queendallen/weather-app", link:""}
 ];
 
