0f473bf13a537bced090ee1a015a46ea6cf34e21
Dyandra Allen <dallen9@binghamton.edu>
2024-10-03 22:30:14 -0400

    beginning changes

---
+++ src/utils/constants.tsx
@@ -0,0 +1,5 @@
+const PROJECTS = [
+  {name: "Portfolio", skills:["React","Typescript", "TailwindCSS"], desc:"Displays my personal projects", githubLink:"https://github.com/queendallen/queendallen.github.io", link:""},
+  {name: "Weather App", skills:["React", "HTML", "CSS"], desc:"Gives 4-day forecast for any inputted location", githubLink:"https://github.com/queendallen/weather-app", link:""},
+  {name: "SA Travel Form", projType: "work", skills:["JS", "HTML", "CSS", "MySQL", "PHP"], desc:"Travel form for Binghamton University's Student Association. Note: No Github Link", githubLink:"", link:"https://www.forms.binghamtonsa.org/travel/"},
+];
